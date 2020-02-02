aaea639721900b3062de2139ed5007a4693242c2
Maryam <maryam.aslam@outlook.com>
2020-02-02 22:10:01 +0100

    packaging to war

--- src/main/java/no/maryam/portfolio/PortfolioApplication.java
+++ src/main/java/no/maryam/portfolio/PortfolioApplication.java
@@ -2,12 +2,19 @@ package no.maryam.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortfolioApplication {
+public class PortfolioApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(PortfolioApplication.class);
     }
 
 }
